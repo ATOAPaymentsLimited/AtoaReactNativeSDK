e9c739d9af57c2a4d6a53c1de6aa28448f0cf052
Vancha Omer <vancha@paywithatoa.co.uk>
2026-02-25 19:09:57 +0530

    fix: bank logos image and connectivity demo_app

--- demo_app/App.tsx
+++ demo_app/App.tsx
@@ -61,7 +61,7 @@ function getRequestData(amount: number) {
 
 async function getPaymentRequestId(amount: number): Promise<string> {
   const response = await fetch(
-   'https://api.atoa.me/api/payments/process-payment',
+    'https://api.atoa.me/api/payments/process-payment',
     {
       method: 'POST',
       headers: {
@@ -170,7 +170,7 @@ function useConnectivity(): {
     return () => unsubscribe();
   }, [checkConnection]);
 
-  const isOffline = status === 'offline' || status === 'waiting';
+  const isOffline = status === 'offline';
 
   return {status, isOffline, checkConnection};
 }
