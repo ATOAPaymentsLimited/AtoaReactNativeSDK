976fc63ef37e2caccbd1ee309c03d5a5bfc6ddf3
AmitG7781 <amit@paywithatoa.co.uk>
2026-03-04 14:27:58 +0530

    fix: figtree font on ios

--- ios/AtoaFontLoader.m
+++ ios/AtoaFontLoader.m
@@ -1,12 +1,18 @@
 #import <Foundation/Foundation.h>
 #import <CoreText/CoreText.h>
 
+// Helper class used solely to locate the correct bundle for this pod.
+// NSClassFromString(@"AtoaAppInstalled") fails under use_frameworks!
+// because Swift classes require module-qualified names.
+@interface AtoaFontLoaderAnchor : NSObject
+@end
+
+@implementation AtoaFontLoaderAnchor
+@end
+
 __attribute__((constructor))
 static void AtoaLoadFonts(void) {
-    NSBundle *classBundle = [NSBundle bundleForClass:NSClassFromString(@"AtoaAppInstalled")];
-    if (!classBundle) {
-        classBundle = [NSBundle mainBundle];
-    }
+    NSBundle *classBundle = [NSBundle bundleForClass:[AtoaFontLoaderAnchor class]];
 
     // Fonts are in the AtoaReactNativeSdkFonts resource bundle (set in podspec)
     NSURL *resourceBundleURL = [classBundle URLForResource:@"AtoaReactNativeSdkFonts" withExtension:@"bundle"];
